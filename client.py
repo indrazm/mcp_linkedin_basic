92045f1636fcf488b70e67e763b7be28d3892b02
Indra Zulfi Mushoddaq <me@indrazm.com>
2025-04-05 22:13:47 +0700

    adding linkedin

---
+++ client.py
@@ -0,0 +1,24 @@
+from dotenv import load_dotenv
+from agents import Agent, Runner
+from agents.mcp import MCPServerStdio, MCPServer
+
+load_dotenv(override=True)
+
+
+async def run(mcp_server: MCPServer):
+    agent = Agent(name="agent", instructions="Use the tools to get linkedin profile", mcp_servers=[mcp_server])
+    result = await Runner.run(agent, "Summarize me linkedin profile of indra-zulfi-mushoddaq-9b607319b",)
+    print(result.final_output)
+
+async def main():
+    async with MCPServerStdio(
+        params={
+            "command": "mcp-proxy",
+            "args": ["http://localhost:8000/mcp"],
+        }
+    ) as server:
+        await run(server)
+        
+if __name__ == "__main__":
+    import asyncio
+    asyncio.run(main())
--- main.py
+++ main.py
@@ -1,6 +1,24 @@
-def main():
-    print("Hello from openaiagents!")
+from fastapi import FastAPI
+from fastapi_mcp import add_mcp_server
+from linkedin_api import Linkedin
+from dotenv import load_dotenv
+import os
+load_dotenv(override=True)
 
+LINKEDIN_USERNAME = os.getenv("LINKEDIN_USERNAME")
+LINKEDIN_PASSWORD = os.getenv("LINKEDIN_PASSWORD")
 
-if __name__ == "__main__":
-    main()
+api = Linkedin(LINKEDIN_USERNAME, LINKEDIN_PASSWORD)
+
+app = FastAPI()
+
+mcp_server = add_mcp_server(
+    app,
+    mount_path="/mcp",
+    name="My API MCP",
+)
+
+@mcp_server.tool()
+async def get_profile(username:str) -> str:
+    profile = api.get_profile(username)
+    return profile
